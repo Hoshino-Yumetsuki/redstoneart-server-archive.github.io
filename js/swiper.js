147589aae6377b50bd9379a86e683970e0bae717
SuSWhW <Q78KG.1ffrj6@outlook.com>
2023-07-16 22:06:32 +0800

    .

--- js/swiper.js
+++ js/swiper.js
@@ -2,6 +2,8 @@ window.addEventListener('load', function () {
     var swiper = new Swiper('.swiper-container', {
         slidesPerView: 1,
         spaceBetween: 30,
+        observer: true,
+        observerParents: true,
         loop: true,
         // 分页器
         pagination: {
@@ -13,5 +15,9 @@ window.addEventListener('load', function () {
             nextEl: '.swiper-button-next',
             prevEl: '.swiper-button-prev',
         },
+        autoplay: {
+            disableOnInteraction: false,
+            delay: 3000,
+        }
     });
 })
